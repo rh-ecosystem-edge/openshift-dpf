d47ef3ed3cda17bf798e7191880364cdad4b7375
José Castillo Lema <josecastillolema@gmail.com>
2026-07-15 15:01:56 +0200

    Revert "Enable multinetworking on host cluster"
    This reverts commit 9e5ccac37545aa9b348877ecf6ad46284980314a.

--- scripts/dpf.sh
+++ scripts/dpf.sh
@@ -542,10 +542,6 @@ function apply_dpf() {
     log "INFO" "Enabling IP forwarding for OVN Kubernetes..."
     oc patch network.operator.openshift.io cluster --type=merge -p \
     '{"spec":{"defaultNetwork":{ "ovnKubernetesConfig":{"gatewayConfig":{"ipForwarding":"Global"}}}}}'
-
-    log "INFO" "Enabling MultiNetworkPolicy on the cluster network operator..."
-    oc patch network.operator.openshift.io cluster --type=merge -p \
-    '{"spec":{"useMultiNetworkPolicy":true}}'
     
     deploy_nfd
     
